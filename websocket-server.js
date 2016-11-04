90b93341bb74415489f4f0d7ca3760836c879604
John Kottis <johnkottis@gmail.com>
2016-11-04 10:59:38 +0000

    Minor lint changes
    Reindent file

--- websocket-server.js
+++ websocket-server.js
@@ -1,13 +1,13 @@
-var WebSocketServer = require('websocket').server;
-var http = require('http');
-var clients = [];
+var WebSocketServer = require('websocket').server,
+    http = require('http'),
+    clients = [],
+    server = http.createServer(function(request, response) {
+        // process HTTP request. Since we're writing just WebSockets server
+        // we don't have to implement anything.
+    });
 
-var server = http.createServer(function(request, response) {
-    // process HTTP request. Since we're writing just WebSockets server
-    // we don't have to implement anything.
-});
-server.listen(1337, function() {
-  console.log((new Date()) + " Server is listening on port 1337");
+server.listen(1337, function () {
+    console.log((new Date()) + " Server is listening on port 1337");
 });
 
 // create the server
@@ -15,18 +15,27 @@ wsServer = new WebSocketServer({
     httpServer: server
 });
 
-function sendCallback(err) {
-    if (err) console.error("send() error: " + err);
+/**
+ * Notifies when an error occurs
+ * 
+ * @param  {String} err          - Error Response
+ *
+ **/
+function sendCallback (err) {
+    if (err) {
+        console.error("send() error: " + err);
+    }
 }
 
 // This callback function is called every time someone
 // tries to connect to the WebSocket server
-wsServer.on('request', function(request) {
+wsServer.on('request', function (request) {
     console.log((new Date()) + ' Connection from origin ' + request.origin + '.');
     var connection = request.accept(null, request.origin);
+
     console.log(' Connection ' + connection.remoteAddress);
     clients.push(connection);
-    
+
     // This is the most important callback for us, we'll handle
     // all messages from users here.
     connection.on('message', function(message) {
@@ -34,16 +43,16 @@ wsServer.on('request', function(request) {
             // process WebSocket message
             console.log((new Date()) + ' Received Message ' + message.utf8Data);
             // broadcast message to all connected clients
-            clients.forEach(function (outputConnection) {
+            clients.forEach(function(outputConnection) {
                 if (outputConnection != connection) {
-                  outputConnection.send(message.utf8Data, sendCallback);
+                    outputConnection.send(message.utf8Data, sendCallback);
                 }
             });
         }
     });
-    
-    connection.on('close', function(connection) {
+
+    connection.on('close', function (connection) {
         // close user connection
-        console.log((new Date()) + " Peer disconnected.");        
+        console.log((new Date()) + " Peer disconnected.");
     });
 });
